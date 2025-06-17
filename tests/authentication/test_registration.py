7fac3a7e3ee6b0c2ebd9202afaa284fc07301bd2
Ruslan8902 <araslanov.r.v@yandex.ru>
2025-06-17 20:59:41 +0500

    Интеграция Allure в автотесты. Практическая работа с Allure аннотациями

--- tests/authentication/test_registration.py
+++ tests/authentication/test_registration.py
@@ -1,13 +1,29 @@
 import pytest
 
+import allure
+from allure_commons.types import Severity
+from tools.allure.tags import AllureTag
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+
 from pages.dashboard.dashboard_page import DashboardPage
 from pages.authentication.registration_page import RegistrationPage
 
 
 @pytest.mark.regression
 @pytest.mark.registration
+@allure.tag(AllureTag.REGRESSION, AllureTag.REGISTRATION)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.AUTHENTICATION)
+@allure.story(AllureStory.REGISTRATION)
+@allure.parent_suite(AllureEpic.LMS)
+@allure.suite(AllureFeature.AUTHENTICATION)
+@allure.sub_suite(AllureStory.REGISTRATION)
 class TestRegistration:
-        def test_successful_registration(self, dashboard_page: DashboardPage, registration_page: RegistrationPage):  # Создаем тестовую функцию
+    @allure.title("Registration with correct email, username and password")
+    @allure.severity(Severity.CRITICAL)
+    def test_successful_registration(self, dashboard_page: DashboardPage, registration_page: RegistrationPage):  # Создаем тестовую функцию
             registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
             registration_page.registration_form_component.fill('user@gmail.com', 'username', 'password')
             registration_page.click_registration_button()
